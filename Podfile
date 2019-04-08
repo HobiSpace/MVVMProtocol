7e45a0c3927e68273fa40ba4f59f90b5edb4f7e1
Hobi <hzj1@meitu.com>
2019-04-08 17:35:09 +0800

    Pod Init

--- Podfile
+++ Podfile
@@ -16,5 +16,4 @@ target 'MVVMProtocol' do
     inherit! :search_paths
     # Pods for testing
   end
-
 end
---
+++ Pods/Target Support Files/Pods-MVVMProtocol/Pods-MVVMProtocol-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MVVMProtocol : NSObject
+@end
+@implementation PodsDummy_Pods_MVVMProtocol
+@end
